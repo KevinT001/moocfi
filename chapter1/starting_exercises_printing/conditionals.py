c5ddc513b3b746c2b148b94ad3170ee76bf24bdf
Kevin Ta <kvnta@yahoo.com>
2025-05-27 11:56:25 -0600

    add prompt

--- chapter1/starting_exercises_printing/conditionals.py
+++ chapter1/starting_exercises_printing/conditionals.py
@@ -18,3 +18,20 @@ if (num < 0):
 if (num >= 0):
      print(f"{statement}", end=" ")
      print (num)    
+     
+
+# prompt 2
+# Please write a program which asks for the user's name. If the name is anything but "Jerry", the program then asks for the number of portions and prints out the total cost. The price of a single portion is 5.90.
+
+# Two examples of the program's execution:
+
+# Sample output
+# Please tell me your name: Kramer
+# How many portions of soup? 2
+# The total cost is 11.8
+# Next please!
+# Sample output
+# Please tell me your name: Jerry
+# Next please!
+
+#solution
