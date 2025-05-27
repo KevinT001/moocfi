81393489bddd882a830c46f1919ebee93436993e
Kevin Ta <kvnta@yahoo.com>
2025-05-27 00:36:04 -0600

    Add solution to exercise

---
+++ chapter1/starting_exercises_printing/multi_lines.py
@@ -0,0 +1,20 @@
+
+# This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. Please fix the program so that the names are printed in the correct order.
+
+# print("Simeoni")
+# print("Juhani")
+# print("Eero")
+# print("Lauri")
+# print("Aapo")
+# print("Tuomas")
+
+
+#solution
+
+brothers = ["Simeoni", "Juhani", "Eero", "Lauri", "Aapo", "Tuomas", "Timo"]
+
+brothers.sort()
+
+for name in brothers:
+    print(name)
+# The code above creates a list of brothers' names, sorts them in alphabetical order, and then prints each name on a new line.
