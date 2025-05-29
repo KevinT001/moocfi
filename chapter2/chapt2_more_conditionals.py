887de0b1324352a6cb3289057fb3a110bc696891
Kevin Ta <kvnta@yahoo.com>
2025-05-29 08:59:53 -0600

    Complete prompt. added solution

--- chapter2/chapt2_more_conditionals.py
+++ chapter2/chapt2_more_conditionals.py
@@ -250,3 +250,15 @@ else:
 # Number: 45
 # FizzBuzz
 
+#Solution 
+number = int(input("Number:"))
+
+if number%3 == 0 and number%5 == 0:
+    print("FizzBuzz")
+
+elif number%3 == 0:
+    print("Fizz")
+
+elif number%5 ==0:
+    print("Buzz")
+    
