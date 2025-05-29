9ce769e3b37fcce879b582f7ce27e097445d0f35
Kevin Ta <kvnta@yahoo.com>
2025-05-28 19:04:42 -0600

    add prompt for problem

--- chapter2/chapt2_more_conditionals.py
+++ chapter2/chapt2_more_conditionals.py
@@ -86,3 +86,24 @@ elif age < age2:
 else:
     print(f"{name} and {name2} are the same age")
 
+#prompt 3
+
+# Please write a program which asks the user for two words. The program should then print out whichever of the two comes alphabetically last.
+
+# You can assume all words will be typed in lowercase entirely.
+
+# Some examples of expected behaviour:
+
+# Sample output
+# Please type in the 1st word: car
+# Please type in the 2nd word: scooter
+# scooter comes alphabetically last.
+# Sample output
+# Please type in the 1st word: zorro
+# Please type in the 2nd word: batman
+# zorro comes alphabetically last.
+# Sample output
+# Please type in the 1st word: python
+# Please type in the 2nd word: python
+# You gave the same word twice.
+
