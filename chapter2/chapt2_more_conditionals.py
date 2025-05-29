d26988b0ce16eb27db51d5f6b6858de5468f73d6
Kevin Ta <kvnta@yahoo.com>
2025-05-29 08:59:14 -0600

    Add new problem prompt

--- chapter2/chapt2_more_conditionals.py
+++ chapter2/chapt2_more_conditionals.py
@@ -231,4 +231,22 @@ elif 80 <= points <= 89:
     print("Grade: 4")
 else:
     print("Grade: 5")
-    
+
+#prompt 
+
+# Please write a program which asks the user for an integer number. If the number is divisible by three, the program should print out Fizz. If the number is divisible by five, the program should print out Buzz. If the number is divisible by both three and five, the program should print out FizzBuzz.
+
+# Some examples of expected behaviour:
+
+# Sample output
+# Number: 9
+# Fizz
+# Sample output
+# Number: 7
+# Sample output
+# Number: 20
+# Buzz
+# Sample output
+# Number: 45
+# FizzBuzz
+
