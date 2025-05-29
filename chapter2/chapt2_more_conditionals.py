0dd521e3c93ebf7b112a1d183167d8d3aaede618
Kevin Ta <kvnta@yahoo.com>
2025-05-28 18:35:00 -0600

    add solution

--- chapter2/chapt2_more_conditionals.py
+++ chapter2/chapt2_more_conditionals.py
@@ -70,3 +70,19 @@ else:
 # Bill and Jean are the same age
 
 #solution
+
+print("Person 1:")
+name= input("Name:")
+age = int(input("Age:"))
+print("Person 2:")
+name2= (input("Name:"))
+age2 = int(input("Age:"))
+
+if age > age2:
+    print(f"The elder is {name}")
+elif age < age2:
+    print(f"The elder is {name2}")
+
+else:
+    print(f"{name} and {name2} are the same age")
+
