54c3e838d93d39526357b29408afab2aaf2b2a62
Kevin Ta <kvnta@yahoo.com>
2025-05-29 15:58:02 -0600

    complete prompt

--- chapter2/chapt2_more_conditionals.py
+++ chapter2/chapt2_more_conditionals.py
@@ -308,3 +308,15 @@ else:
 # 2nd letter: B
 # 3rd letter: A
 # The letter in the middle is B
+
+#solution
+letter1 = input("1st letter:")
+letter2 = input("2nd letter:")
+letter3 = input("3rd letter:")
+
+if letter1 > letter2 < letter3 > letter1 or letter1<letter2>letter3<letter1:
+    print(f"The letter in the middle is {letter1}")
+elif letter1 <letter2 <letter3 or letter1 > letter2 >letter3:
+    print(f"The letter in the middle is {letter2}")
+elif letter1 < letter2 > letter3 or letter1 >letter2 < letter3:
+    print(f"The letter in the middle is {letter3}")
