d36ee598c0d0d6fe7a1a1346af5dc156f93e3fe9
Kevin Ta <kvnta@yahoo.com>
2025-05-29 12:20:56 -0600

    add problem prompt

--- chapter2/chapt2_more_conditionals.py
+++ chapter2/chapt2_more_conditionals.py
@@ -288,3 +288,23 @@ if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
     print("That year is a leap year.")
 else:
     print("That year is not a leap year.")
+
+
+#prompt
+
+# Please write a program which asks the user for three letters. The program should then print out whichever of the three letters would be in the middle if the letters were in alphabetical order.
+
+# You may assume the letters will be either all uppercase, or all lowercase.
+
+# Some examples of expected behaviour:
+
+# Sample output
+# 1st letter: x
+# 2nd letter: c
+# 3rd letter: p
+# The letter in the middle is p
+# Sample output
+# 1st letter: C
+# 2nd letter: B
+# 3rd letter: A
+# The letter in the middle is B
