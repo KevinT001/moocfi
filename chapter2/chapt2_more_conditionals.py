b7c0670e0fbbb2ae295d5b67243f3c951ac276c5
Kevin Ta <kvnta@yahoo.com>
2025-05-28 21:16:33 -0600

    Add solution and secondary solution

--- chapter2/chapt2_more_conditionals.py
+++ chapter2/chapt2_more_conditionals.py
@@ -176,4 +176,13 @@ elif name == "Ferdie" or name == "Morty":
 else:
     print("You're not a nephew of any character I know of.")
     
+#Other solution 
+# name = input("Please type in your name:")
+
+# if name in("Huey", "Dewey", "Louie"):
+#     print("I think you might be one of Donald Duck's nephews.")
+# elif name in ("Morty", "Ferdie"):
+#     print("I think you might be one of Mickey Mouse's nephews.")
+# else:
+#     print("You're not a nephew of any character I know of.")
     
