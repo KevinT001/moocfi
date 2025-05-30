8603a892775e38ffc687e5992ca3f7020b6e1f59
Kevin Ta <kvnta@yahoo.com>
2025-05-29 21:46:56 -0600

    Add prompt for password exercise

--- chapter2/chpt2_simple_loops.py
+++ chapter2/chpt2_simple_loops.py
@@ -104,3 +104,18 @@ while True:
 
 print("Now!")
 
+#prompt 3
+
+# Please write a program which asks the user for a password. The program should then ask the user to type in the password again. If the user types in something else than the first password, the program should keep on asking until the user types the first password again correctly.
+
+# Have a look at the expected behaviour below:
+
+# Sample output
+# Password: sekred
+# Repeat password: secret
+# They do not match!
+# Repeat password: cantremember
+# They do not match!
+# Repeat password: sekred
+# User account created!
+
