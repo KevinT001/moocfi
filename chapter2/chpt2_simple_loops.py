49af233f294a977eefbd75541fce17f89e4ca825
Kevin Ta <kvnta@yahoo.com>
2025-05-30 09:54:04 -0600

    add prompt for problem

--- chapter2/chpt2_simple_loops.py
+++ chapter2/chpt2_simple_loops.py
@@ -131,3 +131,22 @@ while password != repeat_pass:
         
     print("They do not match!")
 print("User account created!")
+
+
+#prompt 
+# Please write a program which keeps asking the user for a PIN code until they type in the correct one, which is 4321. The program should then print out the number of times the user tried different codes.
+
+# Sample output
+# PIN: 3245
+# Wrong
+# PIN: 1234
+# Wrong
+# PIN: 0000
+# Wrong
+# PIN: 4321
+# Correct! It took you 4 attempts
+# If the user gets it right on the first try, the program should print out something a bit different:
+
+# Sample output
+# PIN: 4321
+# Correct! It only took you one single attempt!
