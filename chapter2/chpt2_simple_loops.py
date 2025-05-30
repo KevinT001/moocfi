b1a87e482fdb267025e0788a2a4f6d85d0d0ef47
Kevin Ta <kvnta@yahoo.com>
2025-05-30 11:02:36 -0600

    Add prompt

--- chapter2/chpt2_simple_loops.py
+++ chapter2/chpt2_simple_loops.py
@@ -172,4 +172,19 @@ while user_input != pin:
         attempts +=1
     print("Wrong")
     
+
+#prompt
+
+# Please write a program which asks the user for a year, and prints out the next leap year.
+
+# Sample output
+# Year: 2023
+# The next leap year after 2023 is 2024
+# If the user inputs a year which is a leap year (such as 2024), the program should print out the following leap year:
+
+# Sample output
+# Year: 2024
+# The next leap year after 2024 is 2028
+    
+    
     
