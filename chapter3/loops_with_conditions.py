499adb990d7c64bbea8446362b1bb92a8cff200d
Kevin Ta <kvnta@yahoo.com>
2025-05-31 18:08:29 -0600

    Add prompt

--- chapter3/loops_with_conditions.py
+++ chapter3/loops_with_conditions.py
@@ -110,3 +110,17 @@ numbers = 1
 while numbers <= upper_limit:
     print(numbers)
     numbers*=2
+
+#prompt
+
+# Please change the program from the previous exercise so that the user gets to input also the base which is multiplied (in the previous program the base was always 2).
+
+# Sample output
+# Upper limit: 27
+# Base: 3
+# 1
+# 3
+# 9
+# 27
+# Please don't use the value True as the condition of your while loop in this exercise!
+
