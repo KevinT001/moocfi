62f064cf948ede57408418cfeca27a74933a8624
Kevin Ta <kvnta@yahoo.com>
2025-06-07 14:37:49 -0600

    add new exercise

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -45,3 +45,16 @@ elif len(string1) < len(string2):
     print(f"{string2} is longer")
 else:
     print("The strings are equally long")
+    
+    #prompt 3
+    
+#     Please write a program which asks the user for a string. The program then prints out the input string in reversed order, from end to beginning. Each character should be on a separate line.
+
+# An example of expected behaviour:
+
+# Sample output
+# Please type in a string: hiya
+# a
+# y
+# i
+# h
