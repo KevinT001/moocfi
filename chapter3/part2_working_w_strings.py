28aa9866b98108079f78506d67482fabbc9ff23b
Kevin Ta <kvnta@yahoo.com>
2025-06-10 08:58:31 -0600

    Add prompt

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -155,3 +155,8 @@ while string != "":
     string = input("Please type in a string:")
     if string == "":
         break
+    
+    #prompt
+#     Please write a program which asks the user for a string and then prints it out so that exactly 20 characters are displayed. If the input is shorter than 20 characters, the beginning of the line is filled in with * characters.
+
+# You may assume the input string is at most 20 characters long.
