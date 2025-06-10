c1e16b647f967f09e20ecefeeefcff9e39bf6130
Kevin Ta <kvnta@yahoo.com>
2025-06-09 18:40:32 -0600

    add prompt

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -125,3 +125,6 @@ length = width * "#"
 while height > 0 : 
     print(length)
     height -=1
+    
+    #prompt 7
+    # Please write a program which asks the user for strings using a loop. The program prints out each string underlined as shown in the examples below. The execution ends when the user inputs an empty string - that is, just presses Enter at the prompt.
