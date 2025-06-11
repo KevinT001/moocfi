0c2cfb210ea695a35e1ea412911ebcce3a5b2339
Kevin Ta <kvnta@yahoo.com>
2025-06-11 16:54:12 -0600

    add example output

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -214,4 +214,11 @@ if string != "":
     
     #prompt 
     # Please write a program which asks the user to type in a string. The program then prints out all the substrings which begin with the first character, from the shortest to the longest. Have a look at the example below.
+    
 
+
+# Please type in a string: test
+# t
+# te
+# tes
+# test
