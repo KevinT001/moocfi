b554d871d8f5ff0036e0c8236f6fb01e31d14182
Kevin Ta <kvnta@yahoo.com>
2025-06-12 09:09:52 -0600

    add prompt

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -229,3 +229,6 @@ string = input("Please type in a string:")
 for char in range(len(string)):
 
    print(string[:char+1])
+
+#prompt
+# Please write a program which asks the user to type in a string. The program then prints out all the substrings which end with the last character, from the shortest to the longest. Have a look at the example below.
