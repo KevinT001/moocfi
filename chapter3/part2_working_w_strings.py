bff35a040e61aeb31aad7bdb83fe149f5cca86b1
Kevin Ta <kvnta@yahoo.com>
2025-06-12 09:11:01 -0600

    add example output

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -232,3 +232,9 @@ for char in range(len(string)):
 
 #prompt
 # Please write a program which asks the user to type in a string. The program then prints out all the substrings which end with the last character, from the shortest to the longest. Have a look at the example below.
+
+#Please type in a string: test
+# t
+# st
+# est
+# test
