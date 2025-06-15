4289521760c50eb06375084469e6f452780e4264
Kevin Ta <kvnta@yahoo.com>
2025-06-14 23:35:07 -0600

    add example output

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -252,3 +252,13 @@ for char in range(len(string)-1,-1,-1):
 # Please write a program which asks the user to input a string. The program then prints out different messages if the string contains any of the vowels a, e or o.
 
 # You may assume the input will be in lowercase entirely. Have a look at the examples below.
+
+# Please type in a string: hello there
+# a not found
+# e found
+# o found
+# Sample output
+# Please type in a string: hiya
+# a found
+# e not found
+# o not found
