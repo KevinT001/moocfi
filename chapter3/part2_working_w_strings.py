e67e4971e32f72d5e8a90c7aaad7427e49c95ef6
Kevin Ta <kvnta@yahoo.com>
2025-06-16 20:39:13 -0600

    add prompt

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -280,3 +280,9 @@ for char in range(len(string)-1,-1,-1):
 #         print("u found")
 #     else: 
 #         print(f"{char} not found")
+
+#prompt
+
+# Please write a program which asks the user to type in a string and a single character. The program then prints the first three character slice which begins with the character specified by the user. You may assume the input string is at least three characters long. The program must print out three characters, or else nothing.
+
+# Pay special attention to when there are less than two characters left in the string after the first occurrence of the character looked for. In that case nothing should be printed out, and there should not be any indexing errors when executing the program.
