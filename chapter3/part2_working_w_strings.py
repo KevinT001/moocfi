2ce7e580369a090e598ab2087c0090e1d84d5899
Kevin Ta <kvnta@yahoo.com>
2025-06-25 23:50:46 -0600

    save example output

--- chapter3/part2_working_w_strings.py
+++ chapter3/part2_working_w_strings.py
@@ -309,3 +309,12 @@ char = input("Please type in a character:")
 #prompt
 
 # Please make an extended version of the previous program, which prints out all the substrings which are at least three characters long, and which begin with the character specified by the user. You may assume the input string is at least three characters long.
+# Please type in a word: mammoth
+# Please type in a character: m
+# mam
+# mmo
+# mot
+# Sample output
+# Please type in a word: banana
+# Please type in a character: n
+# nan
